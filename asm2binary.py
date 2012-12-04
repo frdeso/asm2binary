a3ab1fc62952ab647353ca1f64e6f6b6c1f91005
Francis Deslauriers <fdeslaur@gmail.com>
2012-12-03 22:44:50 -0500

    bug fix
    Signed-off-by: Francis Deslauriers <fdeslaur@gmail.com>

--- asm2binary.py
+++ asm2binary.py
@@ -105,9 +105,17 @@ def analiserLigneHexa(li, diction):    #
 	return instruction
 
 
+def parsing(content):
+	updatedContent = []
+	for line in content:
+		if not line.strip():
+			continue
+		else:
+			updatedContent.append(line)
+	return updatedContent
 
 fichier = open(sys.argv[1])
-content = fichier.readlines()
+content = parsing(fichier.readlines())
 diction = trouverEtiquettes(content)
 vecteur = "("
 for i in content:
